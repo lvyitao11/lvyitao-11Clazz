3331a42786e63278598984af80d52f0fab3fdfd7
lvyitao <lvyitao521@163.com>
2020-06-03 17:18:15 +0900

    项目初始化

---
+++ idea­git­test/src/Test01.java
@@ -0,0 +1,13 @@
+/**
+ * @author lv
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2020/6/3 17:11
+ * @Copyright © 株式会社多言语系统研究所
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("11111");
+
+    }
+}
